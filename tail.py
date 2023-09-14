524358ad71e92d115478fec2c5029f2496407bfd
kentmartin73 <kent@kentsworld.com>
2023-09-14 23:57:47 +0200

    Add files via upload

--- tail.py
+++ tail.py
@@ -4,10 +4,11 @@ from datetime import datetime, timedelta
 import shlex
 
 class Tailer:
-    def __init__(self, command, maxsize=float("inf"), maxtime=timedelta.max):
+    def __init__(self, command, maxsize=float("inf"), maxtime=timedelta.max, func=sys.stdout.write):
         self.maxsize = maxsize
         self.maxtime = maxtime
         self.command = shlex.split(command)
+        self.func = func
         self.chunk = bytearray()
         if self.maxsize == float("inf") and self.maxtime == timedelta.max:
             raise Exception("maxsize or maxtime must be set")
@@ -20,27 +21,32 @@ class Tailer:
             if output == "" and process.poll() is not None:
                 break
             if output:
-                self.sendchunk(output, self.maxsize, self.maxtime)
+                self.sendchunk(output)
                 # sys.stdout.write(output.decode("utf-8"))
                 # sys.stdout.flush()
             rc = process.poll()
 
-    def sendchunk(self, output, size, maxtime):
+    def sendchunk(self, output):
         self.chunk.extend(output)
         since = datetime.now() - self.lastsent
         if len(self.chunk) < self.maxsize and since < self.maxtime:
             pass
         else:
-            sys.stdout.write(self.chunk.decode("utf-8"))
-            sys.stdout.write(" ")
-            sys.stdout.flush()
+            self.func(self.chunk)
+            # sys.stdout.write(self.chunk.decode("utf-8"))
+            # sys.stdout.write(" ")
+            # sys.stdout.flush()
             self.chunk = bytearray()
             self.lastsent = datetime.now()
     
-    
+def chunkprinter(chunk):    
+    sys.stdout.write(chunk.decode("utf-8"))
+    sys.stdout.write(" ")
+    sys.stdout.flush()
+     
 command = "tail -f /tmp/0"
 # command = ["tail", "-f", "/tmp/0"]
 # t = Tailer(command, maxsize=4, maxtime=timedelta(seconds=2))
 # t = Tailer(command, maxsize=8)
-t = Tailer(command, maxtime=timedelta(seconds=2))
+t = Tailer(command, maxtime=timedelta(seconds=2), func=chunkprinter)
 t.run()
